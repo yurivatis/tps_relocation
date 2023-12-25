2a8f527fd69824966515e097bd5f4c429b69f9f1
Yuri Vatis <yurivatis@gmail.com>
2023-12-25 13:26:56 +0100

    Help message modified

--- HelpWidget.cpp
+++ HelpWidget.cpp
@@ -13,7 +13,7 @@ HelpWidget::HelpWidget(QWidget *parent): QWidget(parent)
 	QFrame *name = new QFrame(this);
 	l->addWidget(name);
 	QHBoxLayout *hl=new QHBoxLayout(name);
-    QString version = QString("<h2> tps_relocation 1.5 </h2>");
+    QString version = QString("<h2> tps_relocation 1.6 </h2>");
 	version.append(" </h2>");
 	QLabel *nameLabel = new QLabel(version, name);
 	hl->addWidget(nameLabel, Qt::AlignLeft|Qt::AlignBottom);
@@ -24,9 +24,7 @@ HelpWidget::HelpWidget(QWidget *parent): QWidget(parent)
 	QVBoxLayout *vl= new QVBoxLayout(w);
 	QLabel *lbl = new QLabel(w);
  	lbl->setText(tr("tps_relocation is a small application allowing visualization and movement <br>"
-					" of all Hacon TPS members located on the 1st floor.<br><br>"
-					"<b>Current limitations:</b> To add a new member or to remove an existing member <br>"
-					"you have to modify attached csv file and to import it again.<br>"));
+					" of all Hacon TPS members located on the 1st floor.<br><br>"));
 	lbl->setTextFormat(Qt::RichText);
 	lbl->setOpenExternalLinks(true);
 	vl->addWidget(lbl);
